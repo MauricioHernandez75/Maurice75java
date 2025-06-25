60e619e4b7fd1daa1e26d32892f851a4b6acdd84
MauricioHernandez <mauricio-hernandez104@std.uni.edu.ni>
2025-06-25 13:18:58 -0600

    Multiplicacion agreagada

--- src/HelloWorld1.java
+++ src/HelloWorld1.java
@@ -2,13 +2,16 @@ public class HelloWorld1 {
     public static void main(String[] args) {
         String nombre = "Mauricio";
 
-        int a = 8;
-        int b = 11;
+        int a = 5;
+        int b = 3;
 
         int suma = a+b;
+        int multiplicación = a * b;
 
         System.out.println("Hola ,"+ nombre + "!");
         System.out.println("La suma de" + a + "+" + b + "es igual a:" + suma);
+
+        System.out.println("La multiplicación de: "+ a + "*" + b + " es " + multiplicación);
         
     }
 }
